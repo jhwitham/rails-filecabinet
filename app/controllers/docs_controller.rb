0de6fa3c4772c3a8761608e8844ed146e5f740e8
John Witham <john.h.witham@gmail.com>
2018-09-10 22:24:22 +0000

    Add show view file to display post content & show, doc_params and before_action

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,9 +1,11 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show, :edit, :update, :destroy]
   
   def index
   end
   
   def show
+    @doc = Doc.find(params[:id])
   end
   
   def new
@@ -32,6 +34,7 @@ class DocsController < ApplicationController
     private
       
       def find_doc
+        @doc = Doc.find(params[:id])
       end
       
       def doc_params
